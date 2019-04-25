e2a333108f3a1d22a5c5761ba5212978b3d05584
Gianmarco <gianmarco.caputo.uk@gmail.com>
2019-04-25 18:54:54 +0100

    Updates

---
+++ precache-manifest.d12717d6b6785e703a80dea8c9508339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34875f36373121ca4d584a7a4bd2423f",
+    "url": "/cheapify/index.html"
+  },
+  {
+    "revision": "1c0fa310e0ddd36bfa0b",
+    "url": "/cheapify/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ca21a0a20e1cec982c73",
+    "url": "/cheapify/static/css/main.c199c9ad.chunk.css"
+  },
+  {
+    "revision": "1c0fa310e0ddd36bfa0b",
+    "url": "/cheapify/static/js/2.191af8bd.chunk.js"
+  },
+  {
+    "revision": "ca21a0a20e1cec982c73",
+    "url": "/cheapify/static/js/main.bf235b17.chunk.js"
+  },
+  {
+    "revision": "0b28f759a423edee9942",
+    "url": "/cheapify/static/js/runtime~main.53fad4fc.js"
+  },
+  {
+    "revision": "661a4564530d7496f24f22828c0f3880",
+    "url": "/cheapify/static/media/food.661a4564.png"
+  }
+]);
